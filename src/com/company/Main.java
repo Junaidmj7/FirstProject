74a9549762628c589b34149ec6075349094d73d4
Junaid0712 <junaid.mj@fcs.world>
2021-12-23 12:43:52 +0530

    Spelling corrected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Juneed Khan");
+        System.out.println("Junaid Khan");
     }
 }
